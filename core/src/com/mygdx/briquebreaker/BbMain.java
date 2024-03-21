5445f1e29f96af598d8c3a4e4ce79a642fb0fd3d
Bilel-Azz <113582489+Bilel-Azz@users.noreply.github.com>
2024-03-21 15:17:47 +0100

    ajout de fichier base

--- core/src/com/mygdx/briquebreaker/BbMain.java
+++ core/src/com/mygdx/briquebreaker/BbMain.java
@@ -1,31 +1,80 @@
-package com.mygdx.briquebreaker;
-
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 
 public class BbMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("IMG_8907.jpg");
-	}
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+    private ShapeRenderer shapeRenderer;
+    private Paddle paddle;
+    private Ball ball;
+    private Array<Brick> bricks;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create () {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+        batch = new SpriteBatch();
+        shapeRenderer = new ShapeRenderer();
+        
+        paddle = new Paddle();
+        ball = new Ball();
+        
+        // Create bricks
+        bricks = new Array<>();
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 10; col++) {
+                Brick brick = new Brick(col * 80 + 10, 450 - row * 20);
+                bricks.add(brick);
+            }
+        }
+    }
+
+    @Override
+    public void render () {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        camera.update();
+        
+        batch.setProjectionMatrix(camera.combined);
+        shapeRenderer.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+        paddle.render(batch);
+        ball.render(batch);
+        for (Brick brick : bricks) {
+            brick.render(shapeRenderer);
+        }
+        batch.end();
+        
+        paddle.update();
+        ball.update();
+
+        // Check collision with paddle
+        if (ball.getBounds().overlaps(paddle.getBounds())) {
+            ball.bounceOffPaddle(paddle);
+        }
+
+        // Check collision with bricks
+        for (Brick brick : bricks) {
+            if (ball.getBounds().overlaps(brick.getBounds())) {
+                ball.bounceOffBrick(brick);
+                bricks.removeValue(brick, true);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        shapeRenderer.dispose();
+    }
 }
+
