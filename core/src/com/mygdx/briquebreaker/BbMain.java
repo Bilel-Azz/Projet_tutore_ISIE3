308b8c0eb45de68bfaae9a35c0a579de3ee05063
Bilel-Azz <113582489+Bilel-Azz@users.noreply.github.com>
2024-05-30 11:12:17 +0200

    modif bilel

--- core/src/com/mygdx/briquebreaker/BbMain.java
+++ core/src/com/mygdx/briquebreaker/BbMain.java
@@ -12,6 +12,12 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Iterator;
+
+import static com.badlogic.gdx.math.MathUtils.random;
 
 
 public class BbMain extends ApplicationAdapter {
@@ -28,6 +34,8 @@ public class BbMain extends ApplicationAdapter {
     private String gameOverMessage;
     private int gameOverScore;
     private String gameOverButton;
+
+    private List<Ball> Balls;
 
 
     private OrthographicCamera camera;
@@ -66,18 +74,13 @@ public class BbMain extends ApplicationAdapter {
         font = new BitmapFont();
         font.setColor(Color.WHITE);
 
-        paddle = new Paddle((Gdx.graphics.getWidth() - PADDLE_WIDTH) / 2, 20, PADDLE_WIDTH, PADDLE_HEIGHT);
-        ballPosition = new Vector2(paddle.x + (paddle.width - BALL_RADIUS * 2) / 2, paddle.y + paddle.height + BALL_RADIUS * 2);
-        ballVelocity = new Vector2(BALL_SPEED, BALL_SPEED);
+        Balls = new ArrayList<Ball>();
 
-        bricks = new Rectangle[BRICK_ROWS][BRICK_COLS];
-        for (int row = 0; row < BRICK_ROWS; row++) {
-            for (int col = 0; col < BRICK_COLS; col++) {
-                bricks[row][col] = new Brick(col * (BRICK_WIDTH + BRICK_MARGIN) + BRICK_MARGIN,
-                        Gdx.graphics.getHeight() - (row + 1) * (BRICK_HEIGHT + BRICK_MARGIN),
-                        BRICK_WIDTH, BRICK_HEIGHT,3);
-            }
-        }
+        paddle = new Paddle((Gdx.graphics.getWidth() - PADDLE_WIDTH) / 2, 20, PADDLE_WIDTH, PADDLE_HEIGHT);
+        Balls.add(new Ball(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, BALL_SPEED, BALL_SPEED));
+
+        bricks = new Rectangle[10][10];
+
     }
 
 
@@ -144,17 +147,28 @@ public class BbMain extends ApplicationAdapter {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(Color.WHITE);
         shapeRenderer.rect(paddle.x, paddle.y, paddle.width, paddle.height);
-        shapeRenderer.circle(ballPosition.x, ballPosition.y, BALL_RADIUS);
 
-        for (int row = 0; row < BRICK_ROWS; row++) {
-            for (int col = 0; col < BRICK_COLS; col++) {
-                if (bricks[row][col] != null) {
-                    Brick brick = (Brick) bricks[row][col];
-                    shapeRenderer.setColor(brick.getColor()); // Utilisation de la couleur de la brique
-                    shapeRenderer.rect(brick.x, brick.y, brick.width, brick.height);
-                }
+        for (Ball ball : Balls) {
+            shapeRenderer.circle(ball.position.x, ball.position.y, BALL_RADIUS);
+        }
+
+
+        Map map = new Map();
+        map.loadMapFromXML("map.xml");
+        int[][] brickDurability = map.getBrickDurability();
+        for (int i = 0; i < brickDurability.length; i++) {
+            for (int j = 0; j < brickDurability[0].length; j++) {
+                Brick brick = new Brick(j * (BRICK_WIDTH + BRICK_MARGIN) + BRICK_MARGIN, Gdx.graphics.getHeight() - (i + 1) * (BRICK_HEIGHT + BRICK_MARGIN), BRICK_WIDTH, BRICK_HEIGHT, brickDurability[i][j]);
+                bricks[i][j] = brick;
+                System.out.println("brickDurability[i][j] = " + brickDurability[i][j]);
+                shapeRenderer.setColor(brick.getColor()); // Utilisation de la couleur de la brique
+                shapeRenderer.rect(brick.x, brick.y, brick.width, brick.height);
+
+
             }
         }
+
+
 
         shapeRenderer.end();
 
@@ -191,87 +205,95 @@ public class BbMain extends ApplicationAdapter {
     }
 
     private void update() {
-        // Update ball position
-        ballPosition.x += ballVelocity.x;
-        ballPosition.y += ballVelocity.y;
 
-        // Vérifier la collision avec le paddle
-        if (ballPosition.y - BALL_RADIUS < paddle.y + paddle.height && ballPosition.y + BALL_RADIUS > paddle.y &&
-                ballPosition.x + BALL_RADIUS > paddle.x && ballPosition.x - BALL_RADIUS < paddle.x + paddle.width) {
+        for (Ball ball : Balls) {
+            ball.position.x += ball.velocity.x;
+            ball.position.y += ball.velocity.y;
 
-            // Calculer le point de collision relatif à la largeur du paddle
-            float hitPosition = (ballPosition.x - paddle.x) / paddle.width;
 
-            // Modifier la vélocité en fonction du point de collision
-            ballVelocity.x = BALL_SPEED * (hitPosition - 0.5f) * 2;  // Ball_SPEED * [-1, 1] for left to right
-            ballVelocity.y *= -1; // Inverser la composante y de la vélocité
+            // Update ball position
+            ball.position.x += ball.velocity.x;
+            ball.position.y += ball.velocity.y;
 
-            // Augmenter légèrement la vitesse de la balle
-            ballVelocity.scl(1.05f);
-        }
+            // Vérifier la collision avec le paddle
+            if (ball.position.y - BALL_RADIUS < paddle.y + paddle.height && ball.position.y + BALL_RADIUS > paddle.y &&
+                    ball.position.x + BALL_RADIUS > paddle.x && ball.position.x - BALL_RADIUS < paddle.x + paddle.width) {
 
-        // Check collision with walls
-        if (ballPosition.x - BALL_RADIUS < 0 || ballPosition.x + BALL_RADIUS > Gdx.graphics.getWidth()) {
-            ballVelocity.x *= -1;
-        }
-        if (ballPosition.y + BALL_RADIUS > Gdx.graphics.getHeight()) {
-            ballVelocity.y *= -1;
-        }
+                // Calculer le point de collision relatif à la largeur du paddle
+                float hitPosition = (ball.position.x - paddle.x) / paddle.width;
 
-        // Check collision with bottom of the screen
-        if (ballPosition.y - BALL_RADIUS < 0) { // Vérifie si la balle est en dessous du bord inférieur de l'écran
-            // Définir les informations pour la fin de partie
-            gameOverMessage = "Vous avez perdu";
-            gameOverScore = score;
-            gameOverButton = "Relancer la partie";
+                // Modifier la vélocité en fonction du point de collision
+                ball.velocity.x = BALL_SPEED * (hitPosition - 0.5f) * 2;  // Ball_SPEED * [-1, 1] for left to right
+                ball.velocity.y *= -1; // Inverser la composante y de la vélocité
 
-            // Changer l'état du jeu en GAME_OVER
-            gameState = GameState.GAME_OVER;
+                // Augmenter légèrement la vitesse de la balle
+                ball.velocity.scl(1.05f);
+            }
 
-            // Réinitialiser les paramètres du jeu
-            score = 0; // Réinitialiser le score
-            ballPosition.set(Gdx.graphics.getWidth() / 2, PADDLE_HEIGHT + BALL_RADIUS * 2); // Réinitialiser la position de la balle
-            ballVelocity.set(BALL_SPEED, BALL_SPEED); // Réinitialiser la vitesse de la balle
-            bricks = new Rectangle[BRICK_ROWS][BRICK_COLS]; // Réinitialiser les briques
-        }
+            // Check collision with walls
+            if (ball.position.x - BALL_RADIUS < 0 || ball.position.x + BALL_RADIUS > Gdx.graphics.getWidth()) {
+                ball.velocity.x *= -1;
+            }
+            if (ball.position.y + BALL_RADIUS > Gdx.graphics.getHeight()) {
+                ball.velocity.y *= -1;
+            }
 
-        // Vérifier la collision avec les briques
-        for (int row = 0; row < BRICK_ROWS; row++) {
-            for (int col = 0; col < BRICK_COLS; col++) {
-                if (bricks[row][col] != null) {
-                    Brick brick = (Brick) bricks[row][col];
-                    if (ballPosition.x + BALL_RADIUS > brick.x && ballPosition.x - BALL_RADIUS < brick.x + brick.width &&
-                            ballPosition.y + BALL_RADIUS > brick.y && ballPosition.y - BALL_RADIUS < brick.y + brick.height) {
+            // Check collision with bottom of the screen
+            if (ball.position.y - BALL_RADIUS < 0 && Balls.size() == 1) {
+                // Définir les informations pour la fin de partie
+                gameOverMessage = "Vous avez perdu";
+                gameOverScore = score;
+                gameOverButton = "Relancer la partie";
 
-                        // Détection de la collision
-                        if (ballVelocity.x > 0 && ballPosition.x - BALL_RADIUS < brick.x) {
-                            // Collision avec le côté gauche de la brique
-                            ballVelocity.x *= -1;
-                        } else if (ballVelocity.x < 0 && ballPosition.x + BALL_RADIUS > brick.x + brick.width) {
-                            // Collision avec le côté droit de la brique
-                            ballVelocity.x *= -1;
+                // Changer l'état du jeu en GAME_OVER
+                gameState = GameState.GAME_OVER;
+
+                // Réinitialiser les paramètres du jeu
+                score = 0; // Réinitialiser le score
+                ball.position.set(Gdx.graphics.getWidth() / 2, PADDLE_HEIGHT + BALL_RADIUS * 2); // Réinitialiser la position de la balle
+                ball.velocity.set(BALL_SPEED, BALL_SPEED); // Réinitialiser la vitesse de la balle
+                bricks = new Rectangle[BRICK_ROWS][BRICK_COLS]; // Réinitialiser les briques
+            }
+
+            // Vérifier la collision avec les briques
+            for (int row = 0; row < BRICK_ROWS; row++) {
+                for (int col = 0; col < BRICK_COLS; col++) {
+                    if (bricks[row][col] != null) {
+                        Brick brick = (Brick) bricks[row][col];
+                        if (ball.position.x + BALL_RADIUS > brick.x && ball.position.x - BALL_RADIUS < brick.x + brick.width &&
+                                ball.position.y + BALL_RADIUS > brick.y && ball.position.y - BALL_RADIUS < brick.y + brick.height) {
+
+                            // Détection de la collision
+                            if (ball.velocity.x > 0 && ball.position.x - BALL_RADIUS < brick.x) {
+                                // Collision avec le côté gauche de la brique
+                                ball.velocity.x *= -1;
+                            } else if (ball.velocity.x < 0 && ball.position.x + BALL_RADIUS > brick.x + brick.width) {
+                                // Collision avec le côté droit de la brique
+                                ball.velocity.x *= -1;
+                            }
+                            if (ball.velocity.y > 0 && ball.position.y - BALL_RADIUS < brick.y) {
+                                // Collision avec le côté supérieur de la brique
+                                ball.velocity.y *= -1;
+                            } else if (ball.velocity.y < 0 && ball.position.y + BALL_RADIUS > brick.y + brick.height) {
+                                // Collision avec le côté inférieur de la brique
+                                ball.velocity.y *= -1;
+                            }
+
+                            // Réduction de la durabilité de la brique et mise à jour du score
+                            brick.reduceDurability();
+                            if (brick.getDurability() <= 0) {
+                                bricks[row][col] = null;
+                                score += 10;
+                            }
+
+                            // Sortir de la boucle pour éviter les collisions multiples avec la même brique
+                            break;
                         }
-                        if (ballVelocity.y > 0 && ballPosition.y - BALL_RADIUS < brick.y) {
-                            // Collision avec le côté supérieur de la brique
-                            ballVelocity.y *= -1;
-                        } else if (ballVelocity.y < 0 && ballPosition.y + BALL_RADIUS > brick.y + brick.height) {
-                            // Collision avec le côté inférieur de la brique
-                            ballVelocity.y *= -1;
-                        }
-
-                        // Réduction de la durabilité de la brique et mise à jour du score
-                        brick.reduceDurability();
-                        if (brick.getDurability() <= 0) {
-                            bricks[row][col] = null;
-                            score += 10;
-                        }
-
-                        // Sortir de la boucle pour éviter les collisions multiples avec la même brique
-                        break;
                     }
                 }
             }
         }
+
     }
 
 
